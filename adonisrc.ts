849411088c3ba2d227b81fb70b419f6b96d1dbdf
Jérémy Hoarau <jeremyhoarau44@gmail.com>
2026-05-13 14:06:10 +0400

    config: update AdonisJS API

--- adonisrc.ts
+++ adonisrc.ts
@@ -47,7 +47,7 @@ export default defineConfig({
       environment: ['repl', 'test'],
     },
     () => import('@adonisjs/core/providers/vinejs_provider'),
-    () => import('@adonisjs/session/session_provider'),
+    // () => import('@adonisjs/session/session_provider'),
     () => import('@adonisjs/shield/shield_provider'),
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/cors/cors_provider'),
--- start/env.ts
+++ start/env.ts
@@ -22,6 +22,16 @@ export default await Env.create(new URL('../', import.meta.url), {
   APP_KEY: Env.schema.secret(),
   APP_URL: Env.schema.string({ format: 'url', tld: false }),
 
+  // Database
+  DB_HOST: Env.schema.string(),
+  DB_PORT: Env.schema.number(),
+  DB_USER: Env.schema.string(),
+  DB_PASSWORD: Env.schema.string(),
+  DB_DATABASE: Env.schema.string(),
+
+  // CORS
+  CORS_ORIGIN: Env.schema.string.optional(),
+
   // Session
-  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database'] as const),
+  // SESSION_DRIVER: Env.schema.enum(['cookie', 'memory', 'database'] as const),
 })
